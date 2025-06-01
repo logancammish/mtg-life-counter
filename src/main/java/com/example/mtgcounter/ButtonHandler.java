d42ac9aa9df8d60330bbb17488492a2f48dec8e2
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2025-06-01 16:11:17 +1200

    program is now functional in first version

--- src/main/java/com/example/mtgcounter/ButtonHandler.java
+++ src/main/java/com/example/mtgcounter/ButtonHandler.java
@@ -2,6 +2,7 @@ package com.example.mtgcounter;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class ButtonHandler {
     @FXML
@@ -9,21 +10,40 @@ public class ButtonHandler {
     @FXML
     private Label rightHealth;
 
-    private void increaseLeft(int change) {
-        int leftHealthValue = AppState.getInstance().get("leftHealth") != null ?
-                (int) AppState.getInstance().get("leftHealth") : 20;
-        int newLeftHealthValue = leftHealthValue + change;
-        AppState.getInstance().set("leftHealth", newLeftHealthValue);
-        leftHealth.setText(Integer.toString(newLeftHealthValue));
+    protected void increase(int change, boolean isLeft, boolean reset) {
+        // Reset life to 20 if reset is true
+        if (reset) {
+            if (isLeft) {
+                AppState.getInstance().set("leftHealth", 20);
+                leftHealth.setText("20");
+            } else {
+                AppState.getInstance().set("rightHealth", 20);
+                rightHealth.setText("20");
+            }
+            return;
+        }
+        // Adjust life total based on the change value
+        if (isLeft) {
+            int leftHealthValue = AppState.getInstance().get("leftHealth") != null ?
+                    (int) AppState.getInstance().get("leftHealth") : 20;
+            int newLeftHealthValue = leftHealthValue + change;
+            AppState.getInstance().set("leftHealth", newLeftHealthValue);
+            leftHealth.setText(Integer.toString(newLeftHealthValue));
+        } else {
+            int rightHealthValue = AppState.getInstance().get("rightHealth") != null ?
+                    (int) AppState.getInstance().get("rightHealth") : 20;
+            int newRightHealthValue = rightHealthValue + change;
+            AppState.getInstance().set("rightHealth", newRightHealthValue);
+            rightHealth.setText(Integer.toString(newRightHealthValue));
+        }
     }
 
     @FXML
-    protected void onLeftButtonClickIncreaseOne() {
-        increaseLeft(1);
-    }
-
-    @FXML
-    protected void onLeftButtonClickIncreaseFive() {
-        increaseLeft(5);
+    protected void onLifeButtonClick(javafx.event.ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+        String[] data = clickedButton.getUserData().toString().split(",");
+        int change = Integer.parseInt(data[0]);
+        boolean isLeft = data[1].equals("left");
+        increase(change, isLeft, (change == 1337));
     }
 }
--- src/main/java/com/example/mtgcounter/Main.java
+++ src/main/java/com/example/mtgcounter/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 320, 340);
+        stage.setTitle("MTG Life Counter");
         stage.setScene(scene);
         stage.show();
     }
